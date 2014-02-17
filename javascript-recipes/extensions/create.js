34f89c1c76b9d43a9595a011d1462aa60ec151f3
Marcos Iglesias <golodhros@gmail.com>
2014-02-16 17:07:22 -0800

    Adding some polyfills

---
+++ javascript-recipes/extensions/create.js
@@ -0,0 +1,14 @@
+// Object create polyfill
+// Ref: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/create
+if (typeof Object.create != 'function') {
+    (function () {
+        var F = function () {};
+        Object.create = function (o) {
+            if (arguments.length > 1) { throw Error('Second argument not supported');}
+            if (o === null) { throw Error('Cannot set a null [[Prototype]]');}
+            if (typeof o != 'object') { throw TypeError('Argument must be an object');}
+            F.prototype = o;
+            return new F;
+        };
+    })();
+}
